f1391dcec6ed6689482d94d64eafecbcea6a05dc
Felix Andreas <felix.andreas@melli.com>
2022-12-01 23:18:00 +0100

    it's calories!

--- 01-felix.py
+++ 01-felix.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
-numbers = [
+calories = [
     sum(map(int, x.split("\n")))
     for x in Path("inputs/01-felix").read_text().split("\n\n")
 ]
 
-print("Part 1:", max(numbers))
-print("Part 2:", sum(sorted(numbers, reverse=True)[:3]))
+print("Part 1:", max(calories))
+print("Part 2:", sum(sorted(calories, reverse=True)[:3]))
