1c0c94fbd6a14147e63aece4b903b6f5e684f9ad
Tung <thanh.tung.phan@mit-emilia.de>
2022-12-02 17:36:05 +0100

    tung-02

--- 02-tung.rs
+++ 02-tung.rs
@@ -1,23 +1,37 @@
 use std::collections::HashMap;
 
-/*
-
+/*      1
+a   =   x   3
+b   >   x   0
+c   <   x   6
+        2
+a   <   y   6
+b   =   y   3
+c   >   y   0
+        3
+a   >   z   0
+b   <   z   6
+c   =   z   3
 
 */
 
 fn main() {
-    let part1 = HashMap::from()
+    let part1 = HashMap::from([
+        ("A X", (4,)),
+        ("B X", (1,)),
+        ("C X", (7,)),
+        ("A Y", (8,)),
+        ("B Y", (5,)),
+        ("C Y", (2,)),
+        ("A Z", (3,)),
+        ("B Z", (9,)),
+        ("C Z", (6,)),
+    ]);
 
-    let mut calories: Vec<i32> = std::fs::read_to_string("inputs/02-tung")
+    let p1: Vec<i32> = std::fs::read_to_string("inputs/02-tung")
         .unwrap()
-        .split("\n\n")
-        .map(|x| {
-            x.split('\n')
-                .map(|x| x.parse::<i32>().unwrap())
-                .sum::<i32>()
-        })
+        .split("\n")
+        .map(|x| part1.get(x).unwrap().0.clone())
         .collect();
-    calories.sort();
-    println!("part1: {}", calories.first().unwrap());
-    println!("part2: {}", calories.into_iter().rev().take(3).sum::<i32>())
+    println!("part1: {:?}", p1.iter().sum::<i32>());
 }
